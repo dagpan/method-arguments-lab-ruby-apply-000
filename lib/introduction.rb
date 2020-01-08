7462b98fd333e1cbca2c85bec063f7721062d102
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-08 00:16:48 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+
+#introduction
+
+
+
+
+#introduction_with_language
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
